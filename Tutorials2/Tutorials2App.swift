f66226c2600bf9b15f6f4ca4d422aec39c070111
Andhar7 <70223638+Andhar7@users.noreply.github.com>
2020-12-20 19:39:19 +0100

    Initial Commit

---
+++ Tutorials2/Tutorials2App.swift
@@ -0,0 +1,17 @@
+//
+//  Tutorials2App.swift
+//  Tutorials2
+//
+//  Created by Andrej Kling on 20.12.20.
+//
+
+import SwiftUI
+
+@main
+struct Tutorials2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
